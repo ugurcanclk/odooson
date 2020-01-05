abea13fefae7626e9d11c7f87ded4f9e34cb8dd4
engin <enginbekem@hotmail.com>
2020-01-05 17:38:36 -0500

    added login definition

--- src/test/java/com/odoo/pages/BasePage.java
+++ src/test/java/com/odoo/pages/BasePage.java
@@ -15,24 +15,32 @@ import java.util.List;
 
 public abstract class BasePage {
 
-    @FindBy(css = "span.title-level-1")
-    public List<WebElement> menuOptions;
+    @FindBy (className = "oe_menu_text")
+    public WebElement sales;
 
-    @FindBy(css = "div[class='loader-mask shown']")
-    @CacheLookup
-    protected WebElement loaderMask;
 
-    @FindBy(css = "h1[class='oro-subtitle']")
-    public WebElement pageSubTitle;
 
-    @FindBy(css = "#user-menu > a")
-    public WebElement userName;
 
-    @FindBy(linkText = "Logout")
-    public WebElement logOutLink;
 
-    @FindBy(linkText = "My User")
-    public WebElement myUser;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public BasePage() {
         PageFactory.initElements(Driver.get(), this);
---
+++ src/test/java/com/odoo/pages/Contacts.java
@@ -0,0 +1,4 @@
+package com.odoo.pages;
+
+public class Contacts {
+}
