fd4a8fdf1bc13a577f6f43030832773cc8e2fcc0
“yasindogmus” <oglumuhsincik@gmail.com>
2020-01-05 17:15:58 -0500

    change dryrun to false

--- src/test/java/com/odoo/runners/CukesRunner.java
+++ src/test/java/com/odoo/runners/CukesRunner.java
@@ -9,8 +9,9 @@ import org.junit.runner.RunWith;
         plugin = {"json:target/cucumber.json","html:target/default-cucumber-reports"},
         features = "src/test/resources/features",  //buradaki siralama onemli degil  // features = "src/test/resources/features/Login.feature",
         glue = "com/vytrack/step_definitions",
-        dryRun = false,
-        tags = "@parametre"
+        dryRun = true,
+        tags = "@par"
+
 )
 public class CukesRunner {
 }
